7b3e936f9dd1f3ea1f6210886ea12af027cf2a9f
hugo fortunier <hfortunier@gmail.com>
2021-12-16 17:43:24 +0100

    wip

--- app/javascript/controllers/list_controller.js
+++ app/javascript/controllers/list_controller.js
@@ -1,24 +1,24 @@
-import { Controller } from "stimulus";
+// import { Controller } from "stimulus";
 
-export default class extends Controller {
-  static targets = ['element'];
+// export default class extends Controller {
+//   static targets = ['element'];
 
-  // connect() {
-  //   console.log(this.elementTarget);
-  // }
+//   // connect() {
+//   //   console.log(this.elementTarget);
+//   // }
 
-  updateElement(event) {
-    //updater la db avec checklist true/false
-    console.log(event)
-  const url = this.elementTarget.action
-  fetch(url,  {
-    method: 'PATCH',
-    headers: { 'Accept': 'text/html' },
-    body:
-  })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-  }
-}
+//   updateElement(event) {
+//     //updater la db avec checklist true/false
+//     console.log(event)
+//   const url = this.elementTarget.action
+//   fetch(url,  {
+//     method: 'PATCH',
+//     headers: { 'Accept': 'text/html' },
+//     body:
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     })
+//   }
+// }
